9e49ec39fb6480da96c81421925ef5ce4575268f
Samuel <Samuel@Samuel-OCTOPUSS>
2020-10-14 20:44:04 -0300

    Armando el Login

---
+++ src/cl/duoc/pty/feriavirtualdesktop/entidades/Productor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.duoc.pty.feriavirtualdesktop.entidades;
+
+/**
+ *
+ * @author s1mu2
+ */
+public class Productor {
+    
+}
